ff0d7153b7f12ee0773762f01d2cddb3cc54993f
JAMiles-Gla <2259701m@student.gla.ac.uk>
2024-07-19 13:16:21 +0100

    Update likelihood_calculations.py
    Changes from previous version noted at the bottom of the code :)

--- likelihood_calculations.py
+++ likelihood_calculations.py
@@ -1,17 +1,16 @@
 import numpy as np
 import math
-from scipy import stats
+from pandas import DataFrame, Series
 
 # local imports
-from coordinate_conversions import solarSystemBarycentre, angularMidpoint
-from similarity_checks import similarityDistance
-from time_functions import timeLocations
-from trigger_search import search
-from statistics import combinations
-from plotting_functions import plotTimeLocs, histListLengths
+from GravitationalWaveSETI.coordinate_conversions import solarSystemBarycentre, angularMidpoint
+from GravitationalWaveSETI.similarity_checks import similarityDistance
+from GravitationalWaveSETI.time_functions import timeLocations
+from GravitationalWaveSETI.trigger_search import search
+from GravitationalWaveSETI.plotting_functions import plotTimeLocs, histListLengths
 
 
-def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=None, paramMidpoints=None, paramWindows=None, plot=False, verbose=False):
+def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, activeFraction = 1, minDelta = None, params=None, paramMidpoints=None, paramWindows=None, plot=False, verbose=False):
   '''
   Function defines trigger pairs and loops through rest of dataframe
   to determine triggers that are in sequence.
@@ -22,7 +21,12 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
               sequence:        sequence function (e.g.: primes, Fibonacci, ...)
               maxSeq:          max number of steps before sequence restarts
 
-  Optional:   params:          list of other parameters to check similarity on,
+  Optional:   activeFraction:  The fraction of time you detector (or detector array)
+                               was active for between your first datapoint and your last
+              minDelta:        minimum time separation, in seconds, that you would like
+                               your sequences points to be created
+                               (lower minDelta can lead to long run times)
+              params:          list of other parameters to check similarity on,
                                given as a list of strings
               paramMidpoints:  midpoint in parameter space with which to compare
                                the parameter space of new triggers
@@ -50,8 +54,21 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
     # initialise list plotting variable, if requested
     plotList = []
 
-  # rescale all times to Solar System Barycentre
-  df = solarSystemBarycentre(dataframe)
+  if distanceWindow == None:  ## adhawk way of removing the similarity distance parameter
+    distanceWindow = 360 #the whole sky so that all combinations of points are accepted
+
+    df = dataframe
+
+    df = df.rename(columns={'time0': 'baryTime'})  # thus no solar system baryTime will be needed
+    df['phi0'] = Series(0, index=df.index)
+    df['theta0'] = Series(0, index=df.index)
+
+  else:  # code proceeds as originally designed
+    # rescale all times to Solar System Barycentre
+    df = solarSystemBarycentre(dataframe)
+
+  df['long0'] = np.where(df['phi0'] > 180, df['phi0'] - 360, df['phi0'])
+  df['lat0'] = 90 - df['theta0']
 
   # sort data by time for clear forward and backward directions for location
   # search
@@ -61,23 +78,21 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
 
   # calculate and store longitude and latitude of each point by converting
   # phi to longitude and theta to latitude
-  df['long0'] = np.where(df['phi0'] > 180, df['phi0'] - 360, df['phi0'])
-  df['lat0'] = 90 - df['theta0']
 
   # minimum and maximum global time
-  minTime = df['baryTime'].min()
-  maxTime = df['baryTime'].max() + 1
-  totalTime = maxTime - minTime
+  minTime = df['baryTime'].min() - timeWindow
+  maxTime = df['baryTime'].max() + timeWindow
+
+  totalTime = (maxTime - minTime) * activeFraction # to compensate in the likelihood ratio calculation for the detector being inactive at times
   print('Total time:', totalTime / (24 * 3600), 'days')
 
   # find minimum sequence step length for computational feasibility
-  minDelta = totalTime / 250
+  if minDelta == None: # allowing for a change in minDelta outwith the function
+    minDelta = totalTime / 250 # for plot etc, should be small enough that in a day we could find a signal the next day
   if verbose == True:
-    print('Min delta:', minDelta)
+    print('Min delta:', minDelta, 'seconds')
 
-  # initialised log likelihood value at each iteration (to avoid calculating it
-  # each time); two true triggers are assigned likelihoods later, hence the -2
-  logLikelihoodInit = - len(df.index - 2) * math.log(totalTime)
+  logLikelihoodInit = 0
 
   # loop over rows (triggers)
   for i in range(len(df.index)-1):
@@ -85,7 +100,7 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
     # loop over every other row (trigger)
     for j in range(i+1, len(df.index)):
 
-      if verbose == True:
+      if verbose:
         print()
         print('i:', i, 'j:', j)
 
@@ -94,7 +109,7 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
       t2 = df.iloc[j]
 
       # check similarity based on given params
-      if verbose == True:
+      if verbose:
         print('Initial similarity check...', end=' ')
       if similarityDistance(t1, t2, distanceWindow, verbose=verbose) == True:
 
@@ -109,40 +124,43 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
         timeLocs, timeWindows, sequenceLocs = timeLocations(t1Time, t2Time, minTime, maxTime, sequence, maxSeq,
                                                             minDelta, timeWindow, plot=plot, verbose=verbose)
 
-        # find log likelihood for intial trigger pair
-        logLikelihoodT1 = stats.norm.logpdf(t1Time, loc=t1Time, scale=timeWindow)
-        logLikelihoodT2 = stats.norm.logpdf(t2Time, loc=t2Time, scale=timeWindow)
-
-        # for injection data
-        #if i == 18 and j == 28:
-          #print(timeLocs)
-          #print(midDist)
+        # initialise log likelihood value with background
+        logLikelihoodStart = logLikelihoodInit
 
         # loop over sequence list
         for seqList, seqWindows in zip(timeLocs, timeWindows):
 
-          # initialise log likelihood value with background
-          logLikelihoodStart = logLikelihoodInit + logLikelihoodT1 + logLikelihoodT2
-
           # search for triggers in each sequence list
           logLikelihood, signalCandidates = search(logLikelihoodStart, totalTime, df,
-                                                   seqList, seqWindows, midDist, distanceWindow,
+                                                   seqList, seqWindows, midDist,distanceWindow,
                                                    params=params, paramMidpoints=paramMidpoints,
                                                    paramWindows=paramWindows, verbose=verbose)
 
           # subtract combination statistic
-          combinStat = combinations(len(seqList)+2, len(df.index))
-          logLikelihood -= math.log(combinStat)
+          combinStat = math.perm(len(df.index)-2,len(seqList))
+
+          if verbose:
+            print( 'The combination statistic is:', combinStat)
+
+          # removes excess likelihood from the many possible combinations
+          # - more combintions implies more chance of randomnly finding a siganl
+          # dividing (or subtracting in log space) by the number of posible combinations has the
+          # effect of marginalising over the unknown time possitions of sequential points,
+          # and meaning sequances with higer point density are less likely.
+          if combinStat > 0: # stops code breaking from taking in log(0)
+              logLikelihood -= math.log(combinStat)
+          else:
+              combinStat = 1e-20 # small number ~0
 
           # add log likelihood to list, specifying by 0 if all triggers come
           # from background and by 1 if some are true triggers
           if logLikelihood != logLikelihoodStart - math.log(combinStat):
-            logLikelihoodValues.append([logLikelihood, i, j, len(seqList) + 2, signalCandidates, 1])
+            logLikelihoodValues.append([logLikelihood, i, j, len(seqList) + 2, signalCandidates, 1, t2Time - t1Time,])
           else:
-            logLikelihoodValues.append([logLikelihood, i, j, len(seqList) + 2, signalCandidates, 0])
+            logLikelihoodValues.append([logLikelihood, i, j, len(seqList) + 2, signalCandidates, 0, t2Time - t1Time,])
 
         # additional operations for plotting time locations
-        if plot == True:
+        if plot:
 
           # extend list of time locations
           plotList.extend(timeLocs)
@@ -154,7 +172,7 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
             # increase number of plots
             numberOfTemplates += len(timeLocs)
 
-  if plot == True:
+  if plot:
 
     # plot list lengths if prompted
     histListLengths(plotList)
@@ -169,5 +187,32 @@ def likelihood(dataframe, distanceWindow, timeWindow, sequence, maxSeq, params=N
   # find maximum likelihood value
   maxLogLikelihood = np.max(np.array(logLikelihoodValues)[:, 0])
 
-  # return list of log likelihoods and maximum likelihood value
-  return np.array(logLikelihoodValues), maxLogLikelihood
+  # create a dataframe to return
+  results = np.transpose(np.array(logLikelihoodValues))
+  LogLikelihoodtable = DataFrame({'logLikelihood_ratio':  results[0],
+                                  'ith_datum':  results[1],
+                                  'jth_datum':  results[2],
+                                  'seq_length':  results[3],
+                                  '#_additional_signal_candidates': results[4],
+                                  'true_trigger_flag': results[5],
+                                  'sequence_period': results[6]})
+  # return data frame and maximum likelihood value
+  return LogLikelihoodtable , maxLogLikelihood
+
+'''
+
+Changes from previous version:
+
+- minDelta changing option
+- changed code output to pandas dataframe
+- ± timeWindow on lower and upper limit on sequence
+- verbose shows the number of combinations of 2 points
+- len of log likeliehoodInit changed, as ordering of brackets was wrong
+- added a functionality where None can be an input for the distance window which means the code dosn't consider sky possitions
+- added a functionality where the fraction of the time the detectors were active can be input into modifying the total time
+- code optimised by removing redundant functions:  stats.norm.logpdf(t1Time, loc=t1Time, scale=timeWindow) appears twice but producess the same value
+- added a column into the results table that gives the difference in time between between the ith and jth points
+- minimum and maximum global times extended by timeWindow
+- logLikelihoodStart was set to 0 as first two data points should not be considered in likelihood calculation
+
+'''
