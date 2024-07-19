d90efe2c49029f34fe8aa5caa9ac9ef220a5f123
JAMiles-Gla <2259701m@student.gla.ac.uk>
2024-07-19 13:33:25 +0100

    Update similarity_checks.py
    Added some optimisation

--- similarity_checks.py
+++ similarity_checks.py
@@ -1,51 +1,99 @@
+import math
+from scipy import stats
+
 # local imports
-from coordinate_conversions import greatCircleDistance
-from uncertainty_windows import windows
+from GravitationalWaveSETI.similarity_checks import similarityParams, similarityDistance
 
 
-def similarityDistance(newTrigger, midDist, distanceWindow, verbose=False):
+def search(logLikelihoodStart, totalTime, triggers, sequenceTimes, timeWindows, midDist, distanceWindow, params=None, paramMidpoints=None, paramWindows=None, verbose=False):
   '''
-  Checks if trigger is within distance uncertainty bounds.
+  Searches for triggers in each input time sequence.
 
-  Params:   newTrigger:        new trigger to check if it lies within the
-                               defined uncertainty window
-            midDist:           median distance coordinate between initial
-                               trigger pair, stored as a dataframe containing
-                               léongitude and latitude
-            distanceWindow:    allowed distance uncertainty window
+  Params:   logLikelihoodStart: default likelihood value if triggers are all from
+                                background except initial two
+            totalTime:          full time range of data segment
+            triggers:           all triggers in data
+            sequenceTimes:      central times around which trigger should be found
+            timeWindows:        allowed uncertainty windows around given times
+            midDist:            coordinates of midpoint of Great Circle distance
+                                between initial trigger pair
+            distanceWindow:     allowed distance uncertainty window (if None then no similarity distance critera is considered
+            params:             list of parameters to check similarity on,
+                                given as a list of strings
+            paramMidpoints:     midpoint in parameter space with which to compare
+                                the parameter space of new triggers
+            paramWindows:       allowed uncertainty windows around given parameters,
+                                contains one value for each parameter
 
-  Returns:  boolean:           similarity check yields either True or False
-  '''
-
-  # check if Great Circle distance is greater than allowed window
-  if greatCircleDistance(newTrigger, midDist, verbose=verbose) > distanceWindow:
-    return False
-
-  return True
-
-
-def similarityParams(newTrigger, params, paramMidpoints, paramWindows):
-  '''
-  Performs similarity check on further parameters besides distance.
-
-  Params:   newTrigger:     trigger to perform similarity check on
-            params:         list of parameters to check similarity for, given
-                            as a list of strings
-            paramMidpoints: midpoint in parameter space with which to compare
-                            the parameter space of new triggers
-            paramWindows:   allowed uncertainty windows around given parameters,
-                            contains one value for each parameter
-
-  Returns:  boolean:        similarity check yields either True or False
+  Returns:  logLikelihood:      cumulative likelihood that a sequence of signals is
+                                extraterrestrial
+            signalCandidates:   number of signal candidates in the sequence
   '''
 
-  # loop over desired parameters
-  for p in params:
+  # initialise log likelihood
+  logLikelihood = logLikelihoodStart
 
-    # check if new trigger lies in an acceptable range compared to midpoint
-    # of original trigger pair
-    if abs(newTrigger[p] - paramMidpoints[p][0]) > 2 * windows[p][0]:
-      return False
+  # initialise number of signal candidates in a sequence
+  signalCandidates = 0
 
-  # if none of the checks evaluate to False
-  return True
+  # loop over time list:
+  for time, window in zip(sequenceTimes, timeWindows):
+
+    # find trigger where distance between trigger and true time location is
+    # minimal
+    triggers['timeDifference'] = abs(triggers['baryTime'] - time)
+    closestTriggerIndex = triggers['timeDifference'].idxmin()
+    closestTrigger = triggers.iloc[closestTriggerIndex]
+
+    # check distance similarity with original triggers
+
+    if verbose == True:
+      print('Similarity check...', end=' ')
+
+    # evaluate Gaussian at time location of closest trigger
+    gaussianStatistic = stats.norm.logpdf(closestTrigger['baryTime'], loc=time, scale=window)
+
+    # additionally check similarity in alternative parameter spaces,
+    # if prompted
+    if params != None:
+      if similarityParams(closestTrigger, params, paramMidpoints, paramWindows) == True:
+
+         # check if statistic is greater than background value
+        if gaussianStatistic > - math.log(totalTime):
+
+          # alert that all checks have been passed
+          if verbose == True:
+            print('Signal candidate!')
+
+          # update number of signal candidates
+          signalCandidates += 1
+
+          # add log likelihood for closest trigger to default sum and make
+          # sure to offset by the background value term again
+          logLikelihood += gaussianStatistic + math.log(totalTime) # why are we adding the background?
+
+    else:
+
+      # check if statistic is greater than background value
+      if gaussianStatistic > - math.log(totalTime):
+
+        # alert that all checks have been passed
+        if verbose == True:
+          print('Signal candidate!')
+          #print('baryTime of candidate:',time)
+        # update number of signal candidates
+        signalCandidates += 1
+
+        # add log likelihood for closest trigger to default sum and make
+        # sure to offset by the background value term again
+        logLikelihood += (gaussianStatistic + math.log(totalTime)) #should this second sign not be a minus?
+
+  # otherwise, return log likelihood value
+  return logLikelihood, signalCandidates
+
+'''
+Change log
+
+- optimisation: similarity has already been checked in likelihood function so no need to check it here
+
+'''
